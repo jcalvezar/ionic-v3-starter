767da9bf5ea89411e6295ca14f5302f7799bddfe
Juan Carlos Alvez <jcalvez@hotmail.com>
2020-06-25 01:29:08 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ionic-v3-starter
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage';
+
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
+import { LoginPage } from '../pages/login/login';
+import { AdherirPage } from '../pages/adherir/adherir';
+import { MisdatosPage } from '../pages/misdatos/misdatos';
+import { MilicenciaPage } from '../pages/milicencia/milicencia';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { JcaserviceProvider } from '../providers/jcaservice/jcaservice';
 
 @NgModule({
   declarations: [
@@ -17,11 +26,17 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+		MisdatosPage,
+		MilicenciaPage,
+		LoginPage,
+		AdherirPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+		HttpClientModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,12 +44,17 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+		MisdatosPage,
+		MilicenciaPage,
+		LoginPage,
+		AdherirPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    JcaserviceProvider
   ]
 })
 export class AppModule {}
